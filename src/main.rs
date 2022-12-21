aac351fcbf6212ba830a4c3838676541ff8fce49
despresj <54477023+despresj@users.noreply.github.com>
2022-12-21 18:59:49 -0500

    add arrays

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use ndarray::arr2;
+use ndarray::{arr2, Array2};
+use rand::distributions::Standard;
+use rand::prelude::*;
 
 fn main() {
-    let a = arr2(&[[1, 2, 3],
-                   [4, 5, 6]]);
+    let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
 
-    let b = arr2(&[[6, 5, 4],
-                   [3, 2, 1]]);
+    let b = arr2(&[[6, 5, 4], [3, 2, 1]]);
 
-    let sum = &a + &b;
+    start_here(&a, &b);
 
-    println!("{}", a);
-    println!("+");
-    println!("{}", b);
-    println!("=");
-    println!("{}", sum);
+    let val: [u64; 8] = StdRng::from_entropy().sample(Standard);
+    println!("f32 from [0, 1): {:?}", val);
 }
 
+fn start_here(a: &Array2<i32>, b: &Array2<i32>) {
+    for number in a {
+        println!("number A {}", number)
+    }
+    for number in b {
+        println!("number B {}", number)
+    }
+}
