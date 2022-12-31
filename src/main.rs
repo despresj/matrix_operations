7ac5102d4a5f260aee4fb3bfa2be3230c5205dd7
despresj <54477023+despresj@users.noreply.github.com>
2022-12-31 08:59:39 -0500

    add matrix multiplication from scratch speed test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use ndarray::Array2;
 use rand::thread_rng;
 use rand_distr::{Distribution, Normal};
-
-fn main() {
-    let a = norm_mat(0., 1., 1, 3);
-    let b = norm_mat(4., 5., 3, 20);
-    println!("{},\n{}", a, b);
-}
+use std::time::Instant;
 
 fn norm_mat(mu: f64, sigma: f64, nrow: usize, ncol: usize) -> Array2<f64> {
     assert!(sigma > 0., "sigma must be greater than zero");
@@ -25,4 +20,46 @@ fn norm_mat(mu: f64, sigma: f64, nrow: usize, ncol: usize) -> Array2<f64> {
     }
 
     matrix
+}
+
+// matrix_multiply is a basic matrix multiplication function
+// that will take in two Array2<f64> types and return and Array2<f64>
+fn matrix_multiply(a: &Array2<f64>, b: &Array2<f64>) -> Array2<f64> {
+    let a_rows = a.nrows();
+    let b_cols = b.ncols();
+    let a_cols = a.ncols();
+
+    assert_eq!(
+        a_rows, b_cols,
+        "Matrix Multiplication is undefined unles a rows is equal to b cols"
+    );
+
+    let mut result = Array2::zeros((a_rows, b_cols));
+
+    for i in 0..a_rows {
+        for j in 0..b_cols {
+            for k in 0..a_cols {
+                result[[i, j]] += a[[i, k]] * b[[k, j]];
+            }
+        }
+    }
+
+    result
+}
+
+fn main() {
+    let dims = 100;
+    let start = Instant::now();
+    let a = norm_mat(0., 1., dims, dims);
+    let b = norm_mat(0., 1., dims, dims);
+    let elapsed = start.elapsed();
+    println!("Elapsed time to create matrixies : {:?}", elapsed / 2);
+
+    let start = Instant::now();
+    let _ = matrix_multiply(&a, &b);
+    println!("Elapsed time from scratch: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    let _ = a.dot(&b);
+    println!("Elapsed time from .dot : {:?}", start.elapsed());
 }
